01ba5c236d95d78a0330a6f35bdc12824348e0f1
Nifemi <deyeminifemi@gmail.com>
2023-07-28 13:51:54 +0100

    Ran prettier on codebase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,26 @@
-import '@/styles/index.scss'
-import type { AppProps } from 'next/app'
+import "@/styles/index.scss";
+import type { AppProps } from "next/app";
 import localFont from "next/font/local";
 
 export const googleSans = localFont({
   src: [
     {
-      path: './fonts/GoogleSansDisplay-Regular.ttf',
-      weight: '400',
-      style: 'normal',
+      path: "./fonts/GoogleSansDisplay-Regular.ttf",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: './fonts/GoogleSansDisplay-Bold.ttf',
-      weight: '700',
-      style: 'normal',
+      path: "./fonts/GoogleSansDisplay-Bold.ttf",
+      weight: "700",
+      style: "normal",
     },
-  ]
-})
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={googleSans.className}>
       <Component {...pageProps} />
     </div>
-  )
+  );
 }
