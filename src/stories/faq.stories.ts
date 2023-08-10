fd8629fc3d8a3a3549affbac208a6765ab7036f8
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-08-10 15:23:26 +0000

    completed faq component

---
+++ src/stories/faq.stories.ts
@@ -0,0 +1,14 @@
+import FAQ from "@/components/FAQ";
+
+export default {
+  tags: ["autodocs"],
+  title: "Components/FAQ",
+  component: FAQ,
+};
+
+export const Default = {
+  args: {
+    question: "How can we get TSticks to do giveaway by morning?",
+    answer: "Very simple. Tell him no ticket for him till he does the giveaway",
+  },
+};
