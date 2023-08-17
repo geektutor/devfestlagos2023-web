f17694b99127a4b8dde46569d6c25cca1857e386
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2023-08-17 14:41:26 +0100

    Edited and deployed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,12 +64,8 @@ export default function Home() {
           </figure>
           <h1 className='c-home__intro__title'>Are you ready for 23x?</h1>
           <p className='c-home__intro__subtext'>
-            We&apos;re back! and it&apos;s about to be the most memorable tech festival <br />
-            yet. Get ready for DevFest Lagos 2023.
+           Wetin you dey find? We never launch.
           </p>
-          <TertiaryButton onClick={() => window.open(ticketsUrl, "_blank")}>
-            Grab Your Early Bird Tickets
-          </TertiaryButton>
         </section>
         <section className='c-home__marquee-wrapper'>
           <div className='c-home__marquee' data-marquee>
@@ -79,118 +75,6 @@ export default function Home() {
                 <p className='c-home__marquee__text'>Watch this space for more information soon</p>
               </div>
             ))}
-          </div>
-        </section>
-        <section className='c-home__recap'>
-          <div className='c-home__recap__title'>
-            Let&apos;s recap DevFest Lagos 2022
-            <figure className='c-home__recap__arrow'>
-              <Image src={arrowDoodle} alt='Recap' fill />
-            </figure>
-          </div>
-          <figure className='c-home__recap__image-1' data-recap-image>
-            <Image src={recap1} alt='Recap Image' fill quality={100} />
-          </figure>
-          <figure className='c-home__recap__image-2' data-recap-image>
-            <Image src={recap2} alt='Recap Image' fill quality={100} />
-          </figure>
-          <figure className='c-home__recap__image-3' data-recap-image>
-            <Image src={recap3} alt='Recap Image' fill quality={100} />
-          </figure>
-          <figure className='c-home__recap__image-4' data-recap-image>
-            <Image src={recap4} alt='Recap Image' fill quality={100} />
-          </figure>
-          <figure className='c-home__recap__image-5' data-recap-image>
-            <Image src={recap5} alt='Recap Image' fill quality={100} />
-          </figure>
-          <figure className='c-home__recap__image-6' data-recap-image>
-            <Image src={recap6} alt='Recap Image' fill quality={100} />
-          </figure>
-        </section>
-        <section className='c-home__feedback'>
-          <p className='c-home__feedback__title'>We do not make empty promises ✋🏾😌🤚🏾</p>
-          <div className='c-home__feedback__body'>
-            <div className='c-home__feedback__body-content'>
-              <div className='c-home__feedback__body__quotes-row' data-marquee>
-                {Array.from({ length: 4 }).map(() => (
-                  <>
-                    <div className='c-home__feedback__body__feedback-t-1'>
-                      <Image src={topTweet1} alt='Devfest Feedback' fill priority />
-                    </div>
-                    <div className='c-home__feedback__body__feedback-t-2'>
-                      <Image src={topTweet2} alt='Devfest Feedback' fill priority />
-                    </div>
-                    <div className='c-home__feedback__body__feedback-t-3'>
-                      <Image src={topTweet3} alt='Devfest Feedback' fill priority />
-                    </div>
-                    <div className='c-home__feedback__body__feedback-t-4'>
-                      <Image src={topTweet4} alt='Devfest Feedback' fill priority />
-                    </div>
-                  </>
-                ))}
-              </div>
-              <div className='c-home__feedback__body__quotes-row' data-marquee>
-                {Array.from({ length: 4 }).map(() => (
-                  <>
-                    <div className='c-home__feedback__body__feedback-b-1'>
-                      <Image src={bottomTweet1} alt='Devfest Feedback' fill />
-                    </div>
-                    <div className='c-home__feedback__body__feedback-b-2'>
-                      <Image src={bottomTweet2} alt='Devfest Feedback' />
-                    </div>
-                    <div className='c-home__feedback__body__feedback-b-3'>
-                      <Image src={bottomTweet3} alt='Devfest Feedback' />
-                    </div>
-                    <div className='c-home__feedback__body__feedback-b-4'>
-                      <Image src={bottomTweet4} alt='Devfest Feedback' />
-                    </div>
-                  </>
-                ))}
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className='c-home__highlights'>
-          <div className='c-home__highlights__body'>
-            <p className='c-home__highlights__title'>
-              Check out the highlights of DevFest Lagos 2022😎
-            </p>
-            <div className='c-home__highlights__youtube'>
-              <HighlightIcon className='c-home__highlights__youtube__highlight-icon' />
-              <YoutubePlayer videoId='7kat5HlPtzU' />
-            </div>
-          </div>
-        </section>
-        <ComingSoonCountdown />
-        <section className='c-home__footer'>
-          <ul className='c-home__footer__links'>
-            <li className='c-home__footer__links__link'>
-              <a href='https://gdg.community.dev/gdg-lagos/' target='_blank'>
-                Join the community
-              </a>
-            </li>
-            <li className='c-home__footer__links__link'>
-              <a href='https://policies.google.com/privacy' target='_blank'>
-                Privacy policy
-              </a>
-            </li>
-          </ul>
-          <Logo className='c-home__footer__logo' />
-          <div className='c-home__footer__social-media'>
-            <p className='c-home__footer__social-media__title'>Follow us on:</p>
-            <ul className='c-home__footer__social-media__links'>
-              {socialMediaLinks.map((link) => (
-                <li key={link.link}>
-                  <a
-                    href={link.link}
-                    target='_blank'
-                    className='c-home__footer__social-media__link'
-                  >
-                    {link.icon}
-                  </a>
-                </li>
-              ))}
-            </ul>
           </div>
         </section>
       </main>
