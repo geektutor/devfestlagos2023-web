7e3fbdde6dc7781185fbfd25cdcdd0934a2ca6e9
Nifemi <deyeminifemi@gmail.com>
2023-08-18 14:32:30 +0100

    Added viewport meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,6 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
         <link rel='alternate icon' href='/favicon.ico' type='image/x-icon' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
       <Component {...pageProps} />
     </div>
