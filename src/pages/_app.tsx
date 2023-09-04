18909440a68ec92f6f46d4b882d14275ce398c16
Nifemi <deyeminifemi@gmail.com>
2023-09-04 10:12:52 +0100

    Added google analytics script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/index.scss";
 import type { AppProps } from "next/app";
 import localFont from "next/font/local";
 import Head from "next/head";
+import Script from "next/script";
 
 export const googleSans = localFont({
   src: [
@@ -30,6 +31,16 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
         <link rel='alternate icon' href='/favicon.ico' type='image/x-icon' />
       </Head>
+      <Script src='https://www.googletagmanager.com/gtag/js?id=G-26MR8PNK9R' />
+      <Script id='google-analytics'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-26MR8PNK9R');
+        `}
+      </Script>
       <Component {...pageProps} />
     </div>
   );
