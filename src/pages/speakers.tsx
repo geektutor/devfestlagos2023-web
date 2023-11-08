b92abcc5e14a2017127341c3f43508c6b476a374
Tobipeters <oluwatobilobapeterschem@gmail.com>
2023-11-08 23:00:12 +0100

    fixed merge conflict

--- src/pages/speakers.tsx
+++ src/pages/speakers.tsx
@@ -10,9 +10,15 @@ import ArrowRight from "@/images/arrow-right-bg-light.svg";
 import FaqSection from "@/components/faq-section/faq-section";
 import { Talks } from "@/components/talks-section/talks";
 import { NoMatterWhat } from "@/components/no-matter-what/no-matter-what";
-import Avatar1 from "@/images/speakers-page/avatars/Avatar-1.svg";
-import Avatar2 from "@/images/speakers-page/avatars/Avatar-20.svg";
-import Avatar3 from "@/images/speakers-page/avatars/Memoji Profile Picture.svg";
+import Avatar1 from "@/images/speakers-page/avatars/sp_avatar_1.png";
+import Avatar2 from "@/images/speakers-page/avatars/sp_avatar_2.png";
+import Avatar3 from "@/images/speakers-page/avatars/sp_avatar_3.png";
+import ThreeDotsDoodle from "@/images/Doodles.png";
+import ArrowDoodle from "@/images/arrow-doodle.png";
+import LogicDoodle from "@/images/Logic.png";
+import PeopleDoodle from "@/images/people-doodle.png";
+import RefreshDoodle from "@/images/repeat-doodle.png";
+import { Speaker } from "@/types/Speaker";
 
 export default function Speakers() {
   return (
@@ -23,53 +29,82 @@ export default function Speakers() {
 
       <main className='speakers-page'>
         <section className='speakers_page__hero'>
-          <h3 className='speakers_page__hero_title'>
-            Seasoned speakers sharing industry expertise
-          </h3>
-          <p className='speakers_page__hero_desc'>
-            We have selected the best of the best to bring you amazing talks in this year&apos;s
-            DevFest
-          </p>
-          <PrimaryButton onClick={() => window.open(ticketsUrl, "_blank")}>
-            Get your ticket &emsp; <ArrowRight />
-          </PrimaryButton>
+          <div className='speakers_page__hero__content'>
+            <h3 className='speakers_page__hero__content_title'>
+              Seasoned speakers sharing industry expertise
+            </h3>
+            <p className='speakers_page__hero__content_desc'>
+              We have selected the best of the best to bring you amazing talks in this year&apos;s
+              DevFest
+            </p>
+            <PrimaryButton onClick={() => window.open(ticketsUrl, "_blank")}>
+              Apply to speak &emsp; <ArrowRight />
+            </PrimaryButton>
 
-          <figure className='speakers_page__hero_avatar1'>
-            <Avatar1 />
-          </figure>
-          <figure className='speakers_page__hero_avatar2'>
-            <Avatar2 />
-          </figure>
-          <figure className='speakers_page__hero_avatar3'>
-            <Avatar3 />
-          </figure>
+            <figure className='speakers_page__hero__content_avatar1'>
+              <Image src={Avatar1} alt='Meemoji devfest' quality={100} />
+            </figure>
+            <figure className='speakers_page__hero__content_avatar2'>
+              <Image src={Avatar2} alt='Meemoji devfest' quality={100} />
+            </figure>
+            <figure className='speakers_page__hero__content_avatar3'>
+              <Image src={Avatar3} alt='Meemoji devfest' quality={100} />
+            </figure>
+
+            <figure className='speakers_page__hero__content__arrow-doodle'>
+              <Image src={ArrowDoodle} alt='People Doodle' fill quality={100} />
+            </figure>
+            <figure className='speakers_page__hero__content__logic-doodle'>
+              <Image src={LogicDoodle} alt='People Doodle' fill quality={100} />
+            </figure>
+            <figure className='speakers_page__hero__content__refresh-doodle'>
+              <Image src={RefreshDoodle} alt='Repeat Doodle' fill quality={100} />
+            </figure>
+            <figure className='speakers_page__hero__content__dots-doodle'>
+              <Image src={ThreeDotsDoodle} alt='Video Doodle' fill quality={100} />
+            </figure>
+            <figure className='speakers_page__hero__content__people-doodle'>
+              <Image src={PeopleDoodle} alt='Video Doodle' fill quality={100} />
+            </figure>
+          </div>
         </section>
         <section className='speakers_page__info'>
-          <div className='speakers_page__info_marquee_front' data-marquee>
-            {Array.from({ length: 32 }).map((_, index) => (
-              <div className='speakers_page__marquee_front__item' key={index} data-marquee-item>
-                <p className='speakers_page__marquee_front__text'>
-                  &emsp; 🔥 &emsp; REGISTER NOW &emsp; LIMITED TICKETS AVAILABLE
-                </p>
+          <div className='speakers_page__info_marquee'>
+            <div className='diagonal_two'>
+              <div className='speakers_page__info_marquee_back' data-marquee>
+                {Array.from({ length: 32 }).map((_, index) => (
+                  <div className='speakers-page__marquee_back__item' key={index} data-marquee-item>
+                    <p className='speakers-page__marquee_back__text'>
+                      &emsp; 🔥 &emsp; REGISTER NOW &emsp; LIMITED TICKETS AVAILABLE
+                    </p>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-          <div className='speakers_page__info_marquee_back' data-marquee>
-            {Array.from({ length: 32 }).map((_, index) => (
-              <div className='speakers-page__marquee_back__item' key={index} data-marquee-item>
-                <p className='speakers-page__marquee_back__text'>
-                  &emsp; 🔥 &emsp; REGISTER NOW &emsp; LIMITED TICKETS AVAILABLE
-                </p>
+            </div>
+
+            <div className='diagonal_one'>
+              <div className='speakers_page__info_marquee_front' data-marquee>
+                {Array.from({ length: 32 }).map((_, index) => (
+                  <div className='speakers_page__marquee_front__item' key={index} data-marquee-item>
+                    <p className='speakers_page__marquee_front__text'>
+                      &emsp; 🔥 &emsp; REGISTER NOW &emsp; LIMITED TICKETS AVAILABLE
+                    </p>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
-          <div className='speakers_page__info_cards_note'>Remember, Remember</div>
-          <ul className='speakers_page__info_cards'>
-            <li className='speakers_page__info_cards_details'>
-              <p className='speakers_page__info_cards_details_caption'>🥂Two Days of Awesomeness</p>
-              <h3 className='speakers_page__info_cards_details_month'>NOVEMBER</h3>
-              <h2 className='speakers_page__info_cards_details_day'>24th and 25th</h2>
-              <div className='speakers_page__info_cards_details_subtext'>
+
+          <div className='card_container container'>
+            <div className='speakers_page__info_cards_note'>Remember, Remember</div>
+            <div className='speakers_page__info_cards'>
+              <div className='speakers_page__info_cards_details'>
+                <p className='speakers_page__info_cards_details_caption'>
+                  🥂Two Days of Awesomeness
+                </p>
+                <h3 className='speakers_page__info_cards_details_month'>NOVEMBER</h3>
+                <h2 className='speakers_page__info_cards_details_day'>24th and 25th</h2>
+
                 <p className='speakers_page__info_cards_details_subtext'>
                   We will be making history, once again, with you
                 </p>
@@ -80,21 +115,22 @@ export default function Speakers() {
                   Get your ticket <ArrowRight />
                 </PrimaryButton>
               </div>
-            </li>
-            <li className='speakers_page__info_cards_venue'>
-              <div className='speakers_page__info_cards_venue_image'>
-                <Image
-                  layout='responsive'
-                  src={LandmarkFrontage}
-                  alt='Venue Image'
-                  objectFit='cover'
-                  quality={100}
-                />
+
+              <div className='speakers_page__info_cards_venue'>
+                <div className='speakers_page__info_cards_venue_image'>
+                  <Image
+                    layout='responsive'
+                    src={LandmarkFrontage}
+                    alt='Venue Image'
+                    objectFit='cover'
+                    quality={100}
+                  />
+                </div>
+                <p className='speakers_page__info_cards_venue_caption'>We will be at the </p>
+                <h1 className='speakers_page__info_cards_venue_text'>Landmark Event Center</h1>
               </div>
-              <p className='speakers_page__info_cards_venue_caption'>We will be at the </p>
-              <h1 className='speakers_page__info_cards_venue_text'>Landmark Event Center</h1>
-            </li>
-          </ul>
+            </div>
+          </div>
         </section>
         <section className='speakers_page__speakers'>
           <div className='speakers_page__speakers_header'>
@@ -102,10 +138,10 @@ export default function Speakers() {
             <DaysTab />
           </div>
           <div className='speakers_page__speakers_list'>
-            {speakers.map((speaker, index) => (
+            {speakers.map((speaker: Speaker, index: number) => (
               <SpeakerCard
                 key={index}
-                speaker={speaker!}
+                speaker={speaker}
                 hasNext
                 hasPrevious
                 onClick={() => console.log("Click")}
