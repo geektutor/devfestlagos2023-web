fd076542429ad31a2d3b2d0da2e4f2d94a1267ad
Tobipeters <oluwatobilobapeterschem@gmail.com>
2023-11-08 08:58:19 +0100

    tried

--- src/components/dp-gen/dp-gen.tsx
+++ src/components/dp-gen/dp-gen.tsx
@@ -1,6 +1,6 @@
 import styles from "./dp.gen.module.scss";
 import React from "react";
-import Logo from "@/images/logo.svg";
+import Logo from "@/images/dp-generator/logo.svg";
 import Image from "next/image";
 import Doodle from "@/images/right-doodle.png";
 import Move from "@/images/move-icon.png";
@@ -10,6 +10,7 @@ import RedoIcon from "@/images/redo-icon.svg";
 import ShareIcon from "@/images/share-icon.svg";
 import { PrimaryButton } from "@/components/button";
 import html2canvas from "html2canvas";
+// import domToImage from "dom-to-image";
 
 interface Props {
   name: string;
@@ -21,6 +22,7 @@ interface Props {
 export const DpGen: React.FC<Props> = ({ name, photo, theme, handleRegenerate }) => {
   const sectionRef = React.useRef<HTMLElement | null>(null);
 
+  // HTML2canvas implementation
   const handleDownload = () => {
     if (sectionRef.current) {
       const sectionElement = sectionRef.current;
@@ -58,6 +60,27 @@ export const DpGen: React.FC<Props> = ({ name, photo, theme, handleRegenerate })
     }
   };
 
+  // domToImage implementation
+  // const handleDownload = () => {
+  //   if (sectionRef.current) {
+  //     const sectionElement = sectionRef.current as HTMLElement;
+
+  //     // Use dom-to-image to capture the section content as an image
+  //     domToImage
+  //       .toPng(sectionElement, { quality: 1.0 }) // Set the image quality to 1.0 for maximum quality
+  //       .then((dataUrl) => {
+  //         // Create a download link for the captured image
+  //         const downloadLink = document.createElement("a");
+  //         downloadLink.href = dataUrl;
+  //         downloadLink.download = `${name}-devfest-2023.png`;
+  //         downloadLink.click();
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error capturing image:", error);
+  //       });
+  //   }
+  // };
+
   const handleRedo = () => handleRegenerate(false);
 
   return (
@@ -91,20 +114,26 @@ export const DpGen: React.FC<Props> = ({ name, photo, theme, handleRegenerate })
               className={styles.img_holder}
             >
               <figure className={styles.doodle_1}>
-                <Image src={Doodle} alt='Doodle' quality={100} />
+                <Image src={Doodle} alt='Doodle' />
               </figure>
               <figure className={styles.doodle_2}>
-                <Image src={Move} alt='Move' quality={100} />
+                <Image src={Move} alt='Move' />
               </figure>
               <figure className={styles.doodle_3}>
-                <Image src={Cup} alt='Cup' quality={100} />
+                <Image src={Cup} alt='Cup' />
               </figure>
               {/* {photo && <Image width={340} height={340} src={photo as string} alt={name} />} */}
               <div className={styles.name}>😌 {name}</div>
             </div>
             <div className={styles.be_there}>WILL BE AT</div>
 
-            <Logo className={styles.logo} />
+            {/* <Image className={styles.img_logo} src={Logo} alt='logo' width={"200"} height={"150"} /> */}
+
+            {/* <div className={styles.img_logo}></div> */}
+
+            <div className={styles.logo_container}>
+              <Logo className={styles.logo} />
+            </div>
 
             <ul className={styles.group}>
               <li className={styles.list}>
