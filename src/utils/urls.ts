9b93619345966ac2fb3f11c6d849772a5b12fe9c
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2023-11-13 10:59:32 +0000

    Updated the DP Gen Link

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,4 +1,4 @@
 export const callForSpeakersURL = "/cfp-2023";
 export const ticketsUrl = "https://bit.ly/dflagos23-ticket";
 export const callForVolunteersURL = "volunteer";
-export const dpGeneratorURL = "https://bit.ly/dflagos23-dp";
+export const dpGeneratorURL = "https://bit.ly/dflagos23-dpgen";
