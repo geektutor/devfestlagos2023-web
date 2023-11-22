45740a4de13b2e42d09c203588194ad8012997be
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2023-11-22 23:45:05 +0000

    Update App Linking

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,25 +1,24 @@
-// App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import { isAndroid, isIOS } from 'react-device-detect';
+import React, { useEffect } from 'react';
 
 const App: React.FC = () => {
-  const getRedirectUrl = () => {
-    if (isAndroid) {
-      return 'https://play.google.com/store/apps/details?id=com.gdglagos.devfestlg';
-    } else if (isIOS) {
-      return 'https://apps.apple.com/us/app/devfest-lagos-23/id6471590430';
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+
+    if (userAgent.includes('android')) {
+      window.location.href = 'https://play.google.com/store/apps/details?id=com.gdglagos.devfestlg';
+    } else if (userAgent.includes('iphone') || userAgent.includes('ipad')) {
+      window.location.href = 'https://apps.apple.com/us/app/devfest-lagos-23/id6471590430';
     } else {
-      return 'https://devfestlagos.com/';
+      // PC or other device, redirect to homepage
+      window.location.href = 'https://devfestlagos.com/';
     }
-  };
+  }, []);
 
   return (
-    <Router>
-      <Route
-        render={() => <Redirect to={getRedirectUrl()} />}
-      />
-    </Router>
+    <div>
+      {/* Your main component content */}
+      <h1>Hello, World!</h1>
+    </div>
   );
 };
 
