2940af172a4c5d543ccd107b86b8320ee689eeb1
Nifemi <deyeminifemi@gmail.com>
2023-11-24 08:29:24 +0100

    Fixed room text rendering

--- src/components/schedule-card.tsx
+++ src/components/schedule-card.tsx
@@ -15,21 +15,20 @@ export const ScheduleCard: FC<Props> = ({ schedule }) => {
 
       <div className='scc__footer'>
         <div className='scc__footer__time'>{schedule.time}</div>
-        {schedule.Room ||
-          (schedule.room && (
-            <div className='scc__footer__venue'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='8'
-                height='8'
-                viewBox='0 0 8 8'
-                fill='none'
-              >
-                <circle cx='4' cy='4' r='4' fill='black' />
-              </svg>
-              {schedule.Room || schedule.room}
-            </div>
-          ))}
+        {(schedule.Room || schedule.room) && (
+          <div className='scc__footer__venue'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='8'
+              height='8'
+              viewBox='0 0 8 8'
+              fill='none'
+            >
+              <circle cx='4' cy='4' r='4' fill='black' />
+            </svg>
+            {schedule.Room || schedule.room}
+          </div>
+        )}
       </div>
     </div>
   );
